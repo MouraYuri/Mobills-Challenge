572499b014cdeeb6a94558147f8f071e27a99731
MouraYuri <mourayuri339@gmail.com>
2020-10-06 20:39:43 -0300

    Initial Commit

---
+++ Mobills Challenge/Views/MainScreen/MainScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreenViewController.swift
+//  Mobills Challenge
+//
+//  Created by Yuri Moura on 06/10/20.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+    
+}
