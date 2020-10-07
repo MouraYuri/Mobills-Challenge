7c6489137fdd304be5dbcb7c674d743f4df9bbc4
MouraYuri <mourayuri339@gmail.com>
2020-10-07 02:41:12 -0300

    label being setted dynammically to segmented control and others adjusts done

--- Mobills Challenge/Views/RegisterTransactions/RegisterTransactionViewController.swift
+++ Mobills Challenge/Views/RegisterTransactions/RegisterTransactionViewController.swift
@@ -9,9 +9,60 @@ import UIKit
 
 class RegisterTransactionViewController: UIViewController {
 
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var valueTextField: UITextField!
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    @IBOutlet weak var labelBelowSegmentedControl: UILabel!
+    
+    @IBOutlet weak var transactionOptionsSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var finalizeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.setupTextFieldsDelegates()
     }
     
+    func setupTextFieldsDelegates(){
+        self.descriptionTextField.delegate = self
+        self.valueTextField.delegate = self
+        self.dateTextField.delegate = self
+    }
+    
+    
+    @IBAction func didChangeSegmentedControlOption(_ sender: UISegmentedControl) {
+        
+        let segmentedControlCurrentStringValue = self.transactionOptionsSegmentedControl.titleForSegment(at: self.transactionOptionsSegmentedControl.selectedSegmentIndex)
+        
+        let newLabelString: String = {
+            switch segmentedControlCurrentStringValue {
+            case "Receita":
+                return "A receita foi recebida?"
+            default:
+                return "A despesa foi paga?"
+            }
+        }()
+        
+        DispatchQueue.main.async {
+            self.labelBelowSegmentedControl.text = newLabelString
+        }
+    }
+    
+    
+    @IBAction func didTapFinalizeButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension RegisterTransactionViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
