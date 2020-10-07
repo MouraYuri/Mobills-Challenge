6d1791554913869395a8d7c649c6b07aef305543
MouraYuri <mourayuri339@gmail.com>
2020-10-06 22:20:45 -0300

    table view reponsivity adjusts finished and cell correctly being mocked

--- Mobills Challenge/Utils/TableViewCells/TransactionsTableViewCell.swift
+++ Mobills Challenge/Utils/TableViewCells/TransactionsTableViewCell.swift
@@ -21,13 +21,14 @@ class TransactionsTableViewCell: UITableViewCell {
     lazy var descriptionLabel: UILabel = {
         let obj = UILabel()
         obj.translatesAutoresizingMaskIntoConstraints = false
+        obj.font = .boldSystemFont(ofSize: 18)
         obj.text = "Lorem Ipsum"
         return obj
     }()
     
-    lazy var dateLabel: UILabel = {
+    lazy var dateAndStatusLabel: UILabel = {
         let obj = UILabel()
-        obj.text = "04/06/2020"
+        obj.text = "04/06/2020 | Pago"
         obj.translatesAutoresizingMaskIntoConstraints = false
         return obj
     }()
@@ -42,7 +43,35 @@ class TransactionsTableViewCell: UITableViewCell {
     func setupConstraints(){
         self.contentView.addSubview(self.transactionImage)
         self.contentView.addSubview(self.descriptionLabel)
-        self.contentView.addSubview(self.dateLabel)
+        self.contentView.addSubview(self.dateAndStatusLabel)
         self.contentView.addSubview(self.valueLabel)
+        
+        NSLayoutConstraint.activate([
+            self.transactionImage.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.transactionImage.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            self.transactionImage.widthAnchor.constraint(equalTo: self.contentView.widthAnchor, multiplier: 0.2),
+            self.transactionImage.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -8)
+        ])
+        
+        NSLayoutConstraint.activate([
+            self.descriptionLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.descriptionLabel.leadingAnchor.constraint(equalTo: self.transactionImage.trailingAnchor, constant: 8),
+            self.descriptionLabel.heightAnchor.constraint(equalTo: self.contentView.heightAnchor, multiplier: 0.5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            self.dateAndStatusLabel.topAnchor.constraint(equalTo: self.descriptionLabel.bottomAnchor, constant: 4),
+            self.dateAndStatusLabel.leadingAnchor.constraint(equalTo: self.descriptionLabel.leadingAnchor),
+            self.dateAndStatusLabel.bottomAnchor.constraint(equalTo: self.transactionImage.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            self.valueLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -8),
+            self.valueLabel.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+        ])
+    }
+    
+    func config(){
+        self.setupConstraints()
     }
 }
--- Mobills Challenge/Views/MainScreen/MainScreenViewController.swift
+++ Mobills Challenge/Views/MainScreen/MainScreenViewController.swift
@@ -18,9 +18,27 @@ class MainScreenViewController: UIViewController {
     }
     
     func setupTableView(){
-        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(TransactionsTableViewCell.self, forCellReuseIdentifier: TransactionsTableViewCell.identifier)
     }
     
 }
 
-
+extension MainScreenViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TransactionsTableViewCell.identifier) as? TransactionsTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.config()
+        return cell
+    }
+}
